150bdb29add0a680e827e750f171312ebd2eb4f9
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-07-03 09:26:44 +0200

    Fix vnc style

--- src/components/VncViewer/VncViewer.styles.ts
+++ src/components/VncViewer/VncViewer.styles.ts
@@ -20,7 +20,7 @@ export const StyledVNCViewerLoader = styled.div`
 `;
 
 export const StyledVNCScreen = styled.iframe`
-  width: "100%",
-  height: "100%",
-  border: 0,
+  width: 100%;
+  height: 100%;
+  border: 0;
 `;
