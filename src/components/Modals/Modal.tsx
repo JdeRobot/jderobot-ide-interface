1deea496a47c08dc53b43c38cc27fac56e3e9fcf
Javier Izquierdo Hernandez <javizqh@pm.me>
2025-07-09 19:27:59 +0200

    Add list fix

--- src/components/Modals/Modal.tsx
+++ src/components/Modals/Modal.tsx
@@ -315,12 +315,13 @@ export const ModalInputDropdown = ({
         ref={ref}
         id={id}
         name={id}
+        list={id + '-list'}
         onChange={onChange}
         placeholder={placeholder}
         {...props}
       />
       <label htmlFor={id}>{placeholder}</label>
-      <datalist>
+      <datalist id={id + '-list'}>
         {entries && entries.map((name) => <option value={name} />)}
       </datalist>
       {description && <div>{description}</div>}
